6748f54b211a2c9859c6f0f762f502fe0c5cedc3
Асадбек* <asadbekme2002@gmail.com>
2022-06-16 10:01:51 +0500

    slider added with swiper js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// Hamburger Menu
 let menuBtn = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .container .navbar');
 
@@ -5,3 +7,26 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+// Swiper JS
+var swiper = new Swiper(".service-slider", {
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        }
+    }    
+});
